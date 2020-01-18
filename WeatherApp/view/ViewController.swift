e7ec13b3e21bfff4d934c1b581d529c9fac78cb4
kalpesh tita <tita.kalpesh@gmail.com>
2020-01-18 11:00:28 +0530

    initial project setup

---
+++ WeatherApp/view/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Apple on 18/01/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
